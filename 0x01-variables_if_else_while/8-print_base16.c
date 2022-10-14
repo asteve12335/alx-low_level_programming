ef9fca6017f9f749cddadebd0c0d6d0a207e277b
asteve12335 <asteve12335@gmail.com>
2022-10-14 09:07:50 -0700

    base 16 characters

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description: prints a-z to standard output
+ * Return: 0 success
+ */
+int main(void)
+{
+	int d;
+	char low;
+
+	for (d = '0'; d <= '9'; d++)
+	{
+		putchar (d);
+	}
+	for (low = 'a'; low <= 'f'; low++)
+	{
+		putchar (low);
+	}
+	return (0);
+}
