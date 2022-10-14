ab22bbef9c8401ff5a5d1e8c6885b5da5c88f53e
asteve12335 <asteve12335@gmail.com>
2022-10-14 09:39:06 -0700

    Combinations 2 digits

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+ * main -Entry point
+ * Return: 0 Success
+ */
+int main(void)
+{
+	int i;
+	int j;
+
+	for (i = 48; i < 57; i++)
+	{
+		for (j = i + 1; j <= 57; j++)
+		{
+			putchar (i);
+			putchar (j);
+			if (j == 57 && i == 56)
+				break;
+			putchar (44);
+			putchar (32);
+		}
+	}
+	putchar ('\n');
+	return (0);
+}
