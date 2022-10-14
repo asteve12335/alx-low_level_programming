bded929664f1c980d3bdeb5db2b8fd7ee31b74ff
asteve12335 <asteve12335@gmail.com>
2022-10-14 08:37:38 -0700

    Numbers 0 to 9

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description: all single values in base 10
+ * Return: 0 success
+ */
+int main(void)
+{
+	int c;
+
+	for (c = 0; c < 10; c++)
+	{
+		putchar(c);
+	}
+	putchar('\n');
+	return (0);
+}
