b98765dd88c06a41f888f16bf33f087b0fc37945
asteve12335 <asteve12335@gmail.com>
2022-10-14 10:24:02 -0700

    Combination 5

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description: prints combination
+ * Return: 0 success
+ */
+int main(void)
+{
+	inti, o;
+
+	for (i = 0; i < 100; i++)
+	{
+		for (o = i + 1; o < 100; o++)
+		{
+			putchar ((i / 10) + '0');
+			putchar ((i % 10) + '0');
+			putchar (' ');
+			putchar ((o / 10) + '0');
+			putchar ((o % 10) + '0');
+
+			if (i == 98 && o == 99)
+				continue;
+
+			putchar (',');
+			putchar (' ');
+		}
+	}
+	putchar ('\n');
+	return (0);
+}
