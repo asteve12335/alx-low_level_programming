ead96789ae1a6deb04493f6df9f015dde2151f5a
asteve12335 <asteve12335@gmail.com>
2022-10-18 08:10:01 -0700

    Lower case alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * main - Entry point
+ *
+ * Description: Prints lowercase alphabet
+ * Return: 0 success
+ */
+int main(void)
+{
+	char letter;
+
+	for (letter = 'a'; letter < 'z'; letter++)
+	{
+		_putchar(letter);
+	}
+	_putchar('\n');
+	return (0);
+}
