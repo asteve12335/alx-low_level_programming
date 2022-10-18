eaa8c5b05a3bf59e96f0c75327f174c543fbf4a6
asteve12335 <asteve12335@gmail.com>
2022-10-18 10:10:56 -0700

    Gets absolute value

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _abs - Entry point
+ * @n: A real number
+ *
+ * Description: Computes absolute value
+ * Return: int success
+ */
+int _abs(int n)
+{
+	int abs_value;
+
+	abs_value = n * (-1)
+	return (abs_value);
+}
