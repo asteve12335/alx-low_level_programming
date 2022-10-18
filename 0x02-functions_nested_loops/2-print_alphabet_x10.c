7e0e6df9716c5a8673035e97c3b8ef1b1a939455
asteve12335 <asteve12335@gmail.com>
2022-10-18 08:48:03 -0700

    Prints lowercase alphabet x10

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,4 +1,4 @@
-include "main.h"
+#include "main.h"
 /**
  * print_alphabet_x10 - Entry point
  *
@@ -19,7 +19,7 @@ void print_alphabet_x10(void)
 			_putchar(letter);
 			letter++;
 		}
-		_putchar('\n');
 		i++;
 	}
+	_putchar('\n')
 }
