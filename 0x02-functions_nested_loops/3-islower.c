4079e1cb5432b5c5f19cf3d614d155cf050d0380
asteve12335 <asteve12335@gmail.com>
2022-10-18 09:19:55 -0700

    Checke for lower or upper case

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * _islower - Entry point
+ * @c: character to check
+ *
+ * Description: checks for lower or upper case
+ * Return: 1 if c is lower
+ * 0 if c is upper
+ */
+int _islower(int c)
+{
+	if (c >= 65 && c <= 90)
+	{
+		return (0);
+	}
+	else if (c >= 97 && c <= 122)
+	{
+		return (1);
+	}
+}
