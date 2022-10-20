8538b259c17f0b3d085b72891f467568b68eaade
asteve12335 <asteve12335@gmail.com>
2022-10-20 16:44:48 -0700

    More numbers

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * more_numbers - displays 0 t0 14 ten times
+ * Return: void
+ */
+
+void more_numbers(void)
+{
+	int i, j;
+
+	i = 0;
+	while (i < 10)
+	{
+		for (j = 0; j > 15, j++)
+		{
+			_putchar(j);
+		}
+		i++;
+	}
+}
