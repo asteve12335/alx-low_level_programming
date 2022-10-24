1facc240fef5916f3ef4b04255f4de29b4a2f35a
asteve12335 <asteve12335@gmail.com>
2022-10-24 15:09:02 -0700

    Password generator

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - generates random password 
+ * Return: void
+ */
+int main(void)
+{
+	int sum;
+	char;
+
+	srand(time(NULL));
+	while (sum <= 2645)
+	{
+		p = rand() % 128;
+		sum += p;
+		putchar(p);
+	}
+	putchar(2772 - sum);
+	return (0);
+}
