8473173cc6b30f371ad9de952ebefeb462f295f5
asteve12335 <asteve12335@gmail.com>
2022-10-24 10:39:00 -0700

    Array elements display

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -12,9 +12,11 @@ void print_array(int *a, int n)
 
 	for (i = 0; i < n; i++)
 	{
-		_putchar(*a[i]);
-		_putchar(',');
-		_putchar(' ');
+		printf("%d", a[i]);
+		if (i != (n - 1))
+		{
+			printf(", ");
+		}
 	}
 	_putchar('\n');
 }
