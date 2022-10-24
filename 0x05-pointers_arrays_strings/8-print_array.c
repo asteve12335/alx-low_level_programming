a87da8c581b940480efbc59664180ee09f1c713c
asteve12335 <asteve12335@gmail.com>
2022-10-24 10:29:36 -0700

    Array elements display

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_array - Prints n elements of an array
+ * @a: array
+ * @n:number of elements to be printed
+ */
+
+void print_array(int *a, int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		_putchar(a[i]);
+		_putchar(',');
+		_putchar(' ');
+	}
+	_putchar('\n');
+}
