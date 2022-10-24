e928961062f222e9b16aca1e802a7de87c0a5fa1
asteve12335 <asteve12335@gmail.com>
2022-10-24 07:16:08 -0700

    Reverses string

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,23 @@
+#include <sting.h>
+#include "main.h"
+
+/**
+ * rev_string - Reverses a string
+ * @s: string
+ * Return: void
+ */
+
+void rev_string(char *s)
+{
+	int i = strlen(s) - 1, j = 0;
+
+	char = tmp;
+	while (i > j)
+	{
+		tmp = s[i];
+		s[i] = s[j];
+		s[j] = tmp;
+		i--;
+		j++;
+	}
+}
