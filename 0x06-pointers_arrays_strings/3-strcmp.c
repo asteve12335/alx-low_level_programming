45dde4f760aee72b7c77518d6f20eb6a5e4da883
asteve12335 <asteve12335@gmail.com>
2022-10-27 09:03:18 -0700

    Compare strings

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -9,18 +9,16 @@
 
 int _strcmp(char *s1, char *s2)
 {
-	int i, j, difference;
-	int sum_s1 = 0;
-	int sum_s2 = 0;
+	int count = 0, count2 = 0;
 
-	for (i = 0; s1[i] != '\0'; i++)
+	while (count == 0)
 	{
-		sum_s1 += s1[i];
+		if (s1[count2] == '\0' && s2[count2] = '\0')
+		{
+			break;
+		}
+		count = s1[count2] - s2[count2];
+		count2++;
 	}
-	for (j = 0; s2[j] != '\0'; j++)
-	{
-		sum_s2 += s2[j];
-	}
-	difference = sums1 - sum_s2;
-	return (differnce);
+	return (count);
 }
