d7a2147d9d117c67aa5766a0993e1d707eec32fe
asteve12335 <asteve12335@gmail.com>
2022-10-31 09:01:43 -0700

    Strchr

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -15,11 +15,11 @@ char *_strchr(char *s, char c)
 	i = 0;
 	while (s[i] != '\0')
 	{
-		if (s[i] == 'c')
+		if (s[i] == c)
 		{
 			return (&s[i]);
 		}
 		i++;
 	}
-	return ('NULL');
+	return (NULL);
 }
