bb7758f528f3d490e17524db3a2d175237bd3901
asteve12335 <asteve12335@gmail.com>
2022-10-31 10:42:50 -0700

    Strspn

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * _strspn - Gets length of a prefix substring
+ * @s: string1
+ * @accept: string2
+ *
+ * Return: unsigned int
+ */
+
+unsigned int _strspn(char *s, char *accept)
+{
+	unsigned int count = 0, supposed_count = 0;
+	int i, j;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0; accept[j] != '\0'; j++)
+		{
+			if (s[i] == accept[j])
+			{
+				count++;
+			}
+		}
+		supposed_count++;
+		if (cont == 0 || count != supposed_count)
+		{
+			break;
+		}
+	}
+	return (count);
+}
