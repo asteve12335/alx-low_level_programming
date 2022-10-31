6a42ce374028b896852c62555b2d07b6bbf7c53e
asteve12335 <asteve12335@gmail.com>
2022-10-31 12:16:59 -0700

    Strpbrk

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,25 @@
+include "main.h"
+/**
+ * _strpbrk - searches for any set of bytes from string2 in string1
+ * @s: string1
+ * @accept: string2
+ *
+ * Return: pointer to byte s or NULL
+ */
+
+char *_strpbrk(char *s, char *accept)
+{
+	int i, j;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0; accept[j] != '\0'; j++)
+		{
+			if (s[i] == accept[j])
+			{
+				return (&s[i]);
+			}
+		}
+	}
+	return (NULL);
+}
