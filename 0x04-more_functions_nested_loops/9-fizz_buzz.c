efb168dbb03c0171e88aca5c9210303e40958a34
asteve12335 <asteve12335@gmail.com>
2022-11-09 12:33:28 -0800

    Catchup

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -7,27 +7,27 @@
 
 void fizz_buzz(void)
 {
-    int i;
+	int i;
 
-    i = 1;
-    while (i < 100)
-    {
-        if (i % 15 == 0)
-        {
-            printf("FizzBuzz ");
-        }
-        else if (i % 5 == 0)
-        {
-            printf("Buzz ");
-        }
-        else if (i % 3 == 0)
-        {
-            printf("Fizz ");
-        }
-        else
-        {
-            printf("%d ", i);
-        }
-        i++;
-    }
+	i = 1;
+	while (i < 100)
+	{
+		if (i % 15 == 0)
+	{
+		printf("FizzBuzz ");
+	}
+	else if (i % 5 == 0)
+	{
+		printf("Buzz ");
+	}
+	else if (i % 3 == 0)
+	{
+		printf("Fizz ");
+	}
+	else
+	{
+		printf("%d ", i);
+	}
+	i++;
+	}
 }
