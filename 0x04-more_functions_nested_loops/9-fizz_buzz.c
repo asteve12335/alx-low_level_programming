1b760a8fd91a2fc6bfa0e829dc97b2576ba83c1a
@steve <93563603+asteve12335@users.noreply.github.com>
2022-11-09 21:23:33 +0100

    Create 9-fizz_buzz.c

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+/**
+*fizz_buzz - displays the game results
+*Returns: nothing
+*/
+
+void fizz_buzz(void)
+{
+    int i;
+
+    i = 1;
+    while (i < 100)
+    {
+        if (i % 15 == 0)
+        {
+            printf("FizzBuzz ");
+        }
+        else if (i % 5 == 0)
+        {
+            printf("Buzz ");
+        }
+        else if (i % 3 == 0)
+        {
+            printf("Fizz ");
+        }
+        else
+        {
+            printf("%d ", i);
+        }
+        i++;
+    }
+}
