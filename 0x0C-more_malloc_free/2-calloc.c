b091fabc16c6cf6bbd8e7fc96df3a8e0e773c788
@steve <93563603+asteve12335@users.noreply.github.com>
2022-11-10 23:56:04 +0100

    Update 2-calloc.c

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -12,6 +12,8 @@
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
 	void *arr;
+	char *array_filler;
+	unsigned int i;
 
 	if (nmemb == 0 || size == 0)
 	{
@@ -23,6 +25,11 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	if (arr == NULL)
 	{
 		exit(0);
+	}
+	array_filler = arr;
+	for (i = 0; i < (size * nmemb); i++)
+	{
+		array_filler[i] = '\0';
 	}
 	return (arr);
 }
