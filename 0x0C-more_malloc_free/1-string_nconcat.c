b8771d7cf12c8e81e595e6c9562c2214e5206535
asteve12335 <asteve12335@gmail.com>
2022-11-10 20:41:43 +0000

    String nconcat

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -15,7 +15,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	unsigned int i, j, k = 0;
 	char *str;
 
-	str = malloc(_strlen(s1) + n + 1);
+	str = malloc(sizeof(*str) * (n + 1));
 
 	if (str == NULL)
 	{
