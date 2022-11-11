7adb60bc81ecfeab19d3904bbe6112cdb027e488
asteve12335 <asteve12335@gmail.com>
2022-11-11 00:10:10 +0000

    Array Range

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -1,6 +1,38 @@
 #include "main.h"
 #include <stdlib.h>
+/**
+ * array_range - creates an array of integers
+ * @min: lower inclusive integer boundary
+ * @max: upper inclusive integer boundary
+ * Return: pointer to allocated memory
+ */
 
 int *array_range(int min, int max)
 {
+	int *arr;
+	int nmemb, size, i = 0;
+
+	nmemb = max _ min + 1;
+	size = sizeof(int);
+
+	if (min > max)
+	{
+		return (NULL);
+	}
+
+	arr = calloc(nmemb, size);
+
+	if (arr == NULL)
+	{
+		return (NULL);
+	}
+
+	arr[0] = min;
+	arr[nmemb - 1] = max;
+
+	for (i = 1; arr[i] < max; i++)
+	{
+		arr[i] = arr[i - 1] + 1;
+	}
+	return (arr);
 }
