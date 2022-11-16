84c201aa27c12eba4c8c36ef897967791f9c4795
asteve12335 <asteve12335@gmail.com>
2022-11-16 22:00:34 +0000

    Array iterator

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -3,16 +3,17 @@
 /**
  * array_iterator - acts on each array member
  * @array: an array of characters
- * @zize: an int
+ * @size: an int
  * @action: pointer to a function
  */
 
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
-	int i;
+	size_t i;
 
 	if (array && action)
 	{
 		for (i = 0; i < size; i++)
 			action(array[i]);
 	}
+}
