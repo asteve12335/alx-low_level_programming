09968bb7b76d9e28b90e91e1e2325f0587a8c9ec
@steve <93563603+asteve12335@users.noreply.github.com>
2023-03-23 03:32:48 +0100

    Island Perimeter
    Technical interview preparation

---
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+def island_perimeter(grid: list):
+    """
+    Calculates perimeter of Island
+    1: land
+    0: water
+    """
+
+    width, length = len(grid), len(grid[0])
+    perimeter = 0
+
+    for i in range(width):
+        if i > 99:
+            break
+        for j in range(length):
+            if j > 99:
+                break
+            if grid[i][j] == 1:
+                if grid[i - 1][j] == 0:
+                    perimeter += 1
+                if grid[i][j - 1] == 0:
+                    perimeter += 1
+                if grid[i][j + 1] == 0:
+                    perimeter += 1
+                if grid[i + 1][j] == 0:
+                    perimeter += 1
+    return perimeter
